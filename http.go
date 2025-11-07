4423ca8712aba7c29ea0784dba829820e42418ac
Cole Mickens <cole.mickens@determinate.systems>
2025-11-06 21:55:56 -0600

    http: prevent directory listing

--- http.go
+++ http.go
@@ -7,8 +7,39 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 )
+
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
+
+			return nil, err
+		}
+	}
+
+	return f, nil
+}
 
 func webserver(addr string, netbootDir string, b *Broker, m *Machines) (*http.ServeMux, error) {
 	server := http.NewServeMux()
@@ -18,8 +49,8 @@ func webserver(addr string, netbootDir string, b *Broker, m *Machines) (*http.Se
 	} else if _, err := os.Stat(netbootDir); os.IsNotExist(err) {
 		fmt.Printf("netboot directory does not exist, will not serve it: %s", netbootDir)
 	} else {
-		fs := http.FileServer(http.Dir(netbootDir))
 		server.HandleFunc("/mac/{}", func(w http.ResponseWriter, r *http.Request) {
+			fs := http.FileServer(neuteredFileSystem{http.Dir(netbootDir)})
 			http.StripPrefix("/mac/", fs).ServeHTTP(w, r)
 		})
 	}
