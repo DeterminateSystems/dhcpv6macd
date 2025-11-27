2719e35bb8e3962cb95536bcb60c968ecb34476d
Graham Christensen <graham@grahamc.com>
2025-11-27 00:46:03 -0500

    Dropneutered filesystetm

--- http.go
+++ http.go
@@ -12,41 +12,10 @@ import (
 	"net/http"
 	"os"
 	"path"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
 )
-
-type neuteredFileSystem struct {
-	fs http.FileSystem
-}
-
-func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-	f, err := nfs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if err != nil {
-		return nil, err
-	}
-
-	if s.IsDir() {
-		index := filepath.Join(path, "index.html")
-		if _, err := nfs.fs.Open(index); err != nil {
-			closeErr := f.Close()
-			if closeErr != nil {
-				return nil, closeErr
-			}
-
-			return nil, err
-		}
-	}
-
-	return f, nil
-}
 
 func webserver(netbootDir string, b *Broker, m *Machines) (*http.ServeMux, error) {
 	server := http.NewServeMux()
